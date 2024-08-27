4ecb2d7b5150f51cc0f3c601acc1002cd2cd74bb
GivenCingco <givencingco@gmail.com>
2024-08-27 15:15:57 +0200

    Added remote backend

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = var.bucket_name
+    key            = var.key
+    region         = "us-east-1"
+    dynamodb_table = var.dynamo_table_name
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,6 +17,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  profile = "default"
+  profile = var.profile
 }
 
